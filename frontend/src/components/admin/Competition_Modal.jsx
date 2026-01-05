e487411e3d71e2bd9808169da41701f41abdebfd
lolbhailol-lol <jadhavkaran911@gmail.com>
2026-01-05 16:23:36 +0530

    api changes

--- frontend/src/components/admin/Competition_Modal.jsx
+++ frontend/src/components/admin/Competition_Modal.jsx
@@ -12,7 +12,8 @@ export default function CompetitionModal({ fest, onClose }) {
   const fetchCompetitions = async () => {
     try {
       console.log('Frontend - Fetching competitions for fest:', fest._id);
-      const response = await fetch (`${API_CONFIG.BASE_URL}/admin/fests`,${fest._id}/competitions`, {
+      const response = await fetch (`${API_CONFIG.BASE_URL}/admin/fests/${fest._id}/competitions`, {
+
         headers: {
           Authorization: `Bearer ${localStorage.getItem('admin_token')}`,
         },
