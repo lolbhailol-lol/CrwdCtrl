aef73597d419a946a66585fe102fd1df2a247ccb
lolbhailol-lol <jadhavkaran911@gmail.com>
2026-01-25 13:33:22 +0530

    login and form

--- backend/src/middleware/adminAuth.js
+++ backend/src/middleware/adminAuth.js
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
     }
 
     const token = authHeader.split(' ')[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.ADMIN_JWT_SECRET); // Updated to use ADMIN_JWT_SECRET
 
     req.user = decoded;
     next();
